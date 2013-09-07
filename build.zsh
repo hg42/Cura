9cc93fdbaf728fa54537e0870eef0a1c02de06c8
hg42@gmx.net <hg42@gmx.net>
2013-09-07 15:53:23 +0200

    build.zsh: cleanup function

--- build.zsh
+++ build.zsh
@@ -1,9 +1,14 @@
 #!/bin/zsh
 
-sudo rm --recursive --force CuraEngine Power
-for deb in scripts/linux/Cura-*-debian.deb(N); do
-  sudo rm $deb
-  done
+function cleanup()
+  {
+  sudo rm --recursive --force CuraEngine Power
+  for deb in scripts/linux/Cura-*-debian.deb(N); do
+    sudo rm $deb
+    done
+  }
+
+cleanup
 
 ./package.sh debian
 
@@ -11,3 +16,5 @@ sudo dpkg -i scripts/linux/Cura-*-debian.deb(N) &&
   for f in scripts/linux/Cura-*-debian.deb(N); do
     mv $f ./$(filetime $f)-$(basename $f)
     done
+
+cleanup
