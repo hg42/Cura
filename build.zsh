605b74d4b05522d73e4f43eb1adc1b7bfb53a362
hg42@gmx.net <hg42@gmx.net>
2014-01-19 22:18:30 +0100

    test case for running the slicer

--- build.zsh
+++ build.zsh
@@ -3,10 +3,10 @@
 dir3d=/home/harald/3D-print
 
 args=()
-#args=($dir3d/things/test.stl $dir3d/Cura/simulation.ini)
+args=($dir3d/things/test.stl $dir3d/Cura/simulation.ini)
 args=($dir3d/things/test.stl)
+args=(-s $dir3d/things/test.stl -o $dir3d/things/test.gcode)
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
-#cd Cura
-#python cura.py $object
+
 python Cura/cura.py $args
