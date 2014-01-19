293b26c01961da8e393eb134a1203b480382debf
hg42@gmx.net <hg42@gmx.net>
2014-01-19 22:49:19 +0100

    test cura gui again

--- build.zsh
+++ build.zsh
@@ -3,9 +3,9 @@
 dir3d=/home/harald/3D-print
 
 args=()
-args=($dir3d/things/test.stl $dir3d/Cura/simulation.ini)
-args=($dir3d/things/test.stl)
+args=($dir3d/things/test.stl $dir3d/Cura/simulation-speed150.ini)
 args=(-s $dir3d/things/test.stl -o $dir3d/things/test.gcode)
+args=($dir3d/things/test.stl)
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
